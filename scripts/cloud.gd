1cf2a4aa459a8a2bafa55968553799e18d869c21
Matthew Cles <matthew@cles.dev>
2021-05-17 21:59:56 +0800

    update scripts

--- scripts/cloud.gd
+++ scripts/cloud.gd
@@ -1,11 +1,12 @@
 extends MeshInstance
 
-export var speed:float = 1.0
+export var speed:float = .1
 var reposition_x_amount = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VisibilityNotifier.connect("screen_exited", self, 'reposition')
+	speed -= randf() / 10
 
 func _process(delta):
 	translation.x -= delta * speed
---
+++ scripts/move_left.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+var movement_speed = 1
+
+func _ready():
+	$VisibilityNotifier.connect("screen_exited", self, "reposition")
+	show_one_mesh()
+	
+func show_one_mesh():
+	var meshes = $TreeMeshes.get_children()
+	for mesh in meshes:
+		mesh.visible = false
+	var selected_mesh = meshes[randi() % len(meshes)]
+	selected_mesh.visible = true
+
+func _process(delta):
+	var movement = (Vector3.RIGHT * delta * movement_speed).rotated(Vector3.UP, rotation.y)
+	translation += movement
+
+func reposition():
+	var spawn_points = get_tree().get_nodes_in_group('tree_spawn_point')
+	var spawn_point = spawn_points[randi() % len(spawn_points)]
+	translation = spawn_point.translation
+	show_one_mesh()
