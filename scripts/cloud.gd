4f0ebf3fff0c8d6f2172b324def04101374774b0
Matthew Cles <matthew@cles.dev>
2021-05-18 19:27:29 +0800

    catch unused var

--- scripts/cloud.gd
+++ scripts/cloud.gd
@@ -5,7 +5,7 @@ var reposition_x_amount = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VisibilityNotifier.connect("screen_exited", self, 'reposition')
+	var _connected = $VisibilityNotifier.connect("screen_exited", self, 'reposition')
 	speed -= randf() / 10
 
 func _process(delta):
