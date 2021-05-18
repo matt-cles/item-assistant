f0d89fde0dba39aac5200f749d617132b33f471c
Matthew Cles <matthew@cles.dev>
2021-05-18 20:57:51 +0800

    remove extra whitespace

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -6,10 +6,10 @@ onready var viewport = get_viewport()
 func _ready():
 	var _connected = get_tree().get_root().connect("size_changed", self, "_on_resize")
 	pixelize()
-	
+
 func _on_resize():
 	pixelize()
-	
+
 func pixelize():
 	if use_pixels:
 		viewport.size = Vector2(300, 200)
