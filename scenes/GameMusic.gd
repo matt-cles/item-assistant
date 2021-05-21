dbf13da26cf389a1b2ecb74ac1f6d433272ae514
Matthew Cles <matt@Michelles-MacBook-Pro.local>
2021-05-21 17:15:10 +0800

    update min max volumes

--- scenes/GameMusic.gd
+++ scenes/GameMusic.gd
@@ -1,9 +1,10 @@
 extends AudioStreamPlayer
 
 onready var events = get_tree().get_nodes_in_group('events')[0]
+onready var settings = get_tree().get_nodes_in_group('settings')[0]
 
 func _ready():
 	var _connected = events.connect("set_music_volume", self, "set_music_volume")
 
 func set_music_volume(new_volume):
-	volume_db = clamp(new_volume, -80, 0)
+	volume_db = clamp(new_volume, settings.min_volume, settings.max_volume)
