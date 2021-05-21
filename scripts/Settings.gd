42695386b970db12931e2dd1a66a6c1ea608c92d
Matthew Cles <matt@Michelles-MacBook-Pro.local>
2021-05-21 15:13:00 +0800

    modify difficulty settings (testing)

--- scripts/Settings.gd
+++ scripts/Settings.gd
@@ -4,4 +4,4 @@ class_name Settings
 enum PLAYER_MODEL {RANDOM, MALE, FEMALE}
 
 export (PLAYER_MODEL) var character_model = PLAYER_MODEL.RANDOM
-export var difficulty_increment = .5
+export (float, .1, 1) var difficulty_increment = .5
